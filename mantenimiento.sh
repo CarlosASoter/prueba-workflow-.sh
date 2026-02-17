3cfad1c3733e4d092891b5a0ffaabc7f2cc71a2f
CarlosASoter <cmaraboli@soterguard.com>
2026-02-17 11:06:18 +0100

    Create mantenimiento.sh

---
+++ mantenimiento.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Indentación inconsistente
+directorio_temporal="mi carpeta de backup"
+if [ -d $directorio_temporal ]; then
+        echo "El directorio existe"
+        rm -rf $directorio_temporal
+else
+        mkdir $directorio_temporal
+fi
