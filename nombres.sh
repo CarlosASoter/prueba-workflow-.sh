6fc8a0dce0cba61459b44395d750d137e5b45dfc
Carlos Alexander Marabolí Pedraza <cmaraboli@soterguard.com>
2026-02-17 09:29:47 +0100

    archivos de prueba

---
+++ nombres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Variable definida pero nunca usada (SC2034)
+USUARIO_LOGUEADO=$(whoami)
+
+# Variable usada pero nunca definida (SC2154) - Error de dedo
+echo "Bienvenido, $USUARIO_LOGADO"
+
+# Comparación constante (SC2050)
+if [ "pro" == "pro" ]; then
+    echo "Modo producción"
+fi
